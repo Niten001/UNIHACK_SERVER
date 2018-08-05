e33b640d5586d02c3249a693e582f92f693f4bd0
Niten001 <timartin1069@gmail.com>
2018-08-05 10:57:03 +1000

    Initial commit

---
+++ server.js
@@ -0,0 +1,148 @@
+const io = require('socket.io');
+const server = io.listen(8000);
+
+var players = new Map;
+
+server.on("connection", (socket) => {
+  console.log(`Client connected [id=${socket.id}]`);
+  players.set(socket, new Player(20, 20, 20, 20, "red", socket));
+
+  socket.on("input_down_event", (input) => recieveInput(socket, input, true));
+  socket.on("input_up_event", (input) => recieveInput(socket, input, false));
+
+  socket.on("disconnect", () => {
+        players.delete(socket);
+        console.log(`Client gone [id=${socket.id}]`);
+    });
+});
+
+var map = [
+    0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 ,
+    0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 ,
+    0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 ,
+    0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 ,
+    0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 ,
+    0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 ,
+    0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 ,
+    0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 ,
+    0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 ,
+    0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 ,
+    1 , 1 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 1 , 1 ,
+    1 , 1 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 1 , 1 ,
+    1 , 1 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 1 , 1 ,
+    1 , 1 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 1 , 1 ,
+    1 , 1 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 1 , 1 ,
+    1 , 1 , 1 , 1 , 1 , 1 , 1 , 1 , 1 , 1 , 1 , 1 , 1 , 1 , 1 , 1 , 1 , 1 , 1 , 1 , 1 , 1 , 1 , 1 , 1 , 1 , 1 , 1 , 1 , 1 ,
+    1 , 1 , 1 , 1 , 1 , 1 , 1 , 1 , 1 , 1 , 1 , 1 , 1 , 1 , 1 , 1 , 1 , 1 , 1 , 1 , 1 , 1 , 1 , 1 , 1 , 1 , 1 , 1 , 1 , 1 ,
+    1 , 1 , 1 , 1 , 1 , 1 , 1 , 1 , 1 , 1 , 1 , 1 , 1 , 1 , 1 , 1 , 1 , 1 , 1 , 1 , 1 , 1 , 1 , 1 , 1 , 1 , 1 , 1 , 1 , 1 ,
+    1 , 1 , 1 , 1 , 1 , 1 , 1 , 1 , 1 , 1 , 1 , 1 , 1 , 1 , 1 , 1 , 1 , 1 , 1 , 1 , 1 , 1 , 1 , 1 , 1 , 1 , 1 , 1 , 1 , 1 ,
+    1 , 1 , 1 , 1 , 1 , 1 , 1 , 1 , 1 , 1 , 1 , 1 , 1 , 1 , 1 , 1 , 1 , 1 , 1 , 1 , 1 , 1 , 1 , 1 , 1 , 1 , 1 , 1 , 1 , 1
+];
+
+
+
+function sendMap() {
+    //Send Map to Client
+}
+
+class Player {
+    constructor(height, width, x, y, color, id) {
+        this.height = height;
+        this.width = width;
+        this.x = x;
+        this.y = y;
+        this.vx = 0;
+        this.vy = 0;
+        this.moveLeft = false;
+        this.moveRight = false;
+        this.jump = false;
+        this.botCollide = false;
+        this.topCollide = false;
+        this.leftCollide = false;
+        this.rightCollide = false;
+        this.color = color;
+        this.id = id;
+    }
+
+    update() {
+        if ((this.moveLeft = false) && (this.moveRight = false)) {
+          this.vx = 0;
+        }
+        if ((this.moveLeft = true) && (this.moveRight = false)) {
+          this.x -= 1;
+        }
+        if ((this.moveLeft = false) && (this.moveRight = true)) {
+          this.x += 1;
+        }
+        if ((this.moveLeft = true) && (this.moveRight = true)) {
+          this.vx = 0;
+        }
+
+        /*if ((this.jump = true) && (this.botCollide = true)) {
+          this.vy = 1;
+        }*/
+        console.log(this.x);
+        server.emit("player_update", this.x);
+        //Apply gravity and velocity changes
+        //Check collisions with map and other players
+    }
+
+    checkCollision() {
+        //Check collision with map and other players and direction of collision (can be more than 1 direction)
+    }
+
+}
+
+function recieveInput(ID, inputType, input) {
+    for (const player of players.values()) {
+        if (player.id == ID) {
+            switch (inputType) {
+                case "left":
+                    if (input) {
+            						console.log("left");
+                        player.x -= 1;
+                        player.update();
+                    } else {
+                        player.moveLeft = false;
+                    }
+                    break;
+                case "right":
+                    if (input) {
+            						console.log("right");
+                        player.x += 1;
+                        player.update();
+
+                    } else {
+                        player.moveRight = false;
+                    }
+                    break;
+                case "space":
+                    if (input) {
+            						console.log("space");
+                        player.jump = true;
+                        player.update();
+                    } else {
+                        player.jump = false;
+                    }
+                    break;
+                default:
+                    break;
+            }
+        }
+	}
+}
+
+function runGame() {
+  console.log("game running");
+
+  while (true) {
+    //get inputs
+
+    //update
+      //for all players calc next position
+      // -> calcCollision
+      // -> calcPoints
+
+    //send data to clients
+  }
+}
